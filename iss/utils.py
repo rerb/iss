0e0099289a452b978e1d27807a165f02277c6993
rerb <bob.erb@aashe.org>
2015-10-09 16:53:18 -0400

    Debug update_iss_organizations

--- iss/utils.py
+++ iss/utils.py
@@ -5,41 +5,76 @@ import iss.models
 def update_iss_organizations():
     """Update the ISS Organizations from the issdjango data.
     """
-    for source_org in issdjango.models.Organizations.objects.all():
+    for source in issdjango.models.Organizations.objects.all():
         try:
-            target_org = iss.models.Organization.objects.get(
-                account_num=source_org.account_num)
+            target = iss.models.Organization.objects.get(
+                account_num=source.account_num)
         except iss.models.Organization.DoesNotExist:
-            target_org = iss.models.Organization.objects.create(
-                account_num=source_org.account_num)
+            target = iss.models.Organization(
+                account_num=source.account_num)
 
-        target_org.sf_id = source_org.sf_id
-        target_org.org_name = source_org.org_name
-        target_org.picklist_name = source_org.picklist_name
-        target_org.exclude_from_website = source_org.exclude_from_website
-        target_org.is_defunct = source_org.is_defunct
-        target_org.is_member = source_org.is_member
-        target_org.member_type = source_org.member_type
-        target_org.business_member_level = source_org.business_member_level
-        target_org.sector = source_org.sector
-        target_org.org_type = source_org.org_type
-        target_org.carnegie_class = source_org.carnegie_class
-        target_org.class_profile = source_org.class_profile
-        target_org.setting = source_org.setting
-        target_org.longitude = source_org.longitude
-        target_org.latitude = source_org.latitude
-        target_org.street = source_org.street
-        target_org.city = source_org.city
-        target_org.state = source_org.state
-        target_org.postal_code = source_org.postal_code
-        target_org.country = source_org.country
-        target_org.country_iso = source_org.country_iso
-        target_org.website = source_org.website
-        target_org.sustainability_website = source_org.sustainability_website
-        target_org.enrollment_fte = source_org.enrollment_fte
-        target_org.stars_participant_status = (
-            source_org.stars_participant_status)
-        target_org.pilot_participant = source_org.pilot_participant
-        target_org.is_signatory = source_org.is_signatory
+        target.sf_id = source.sf_id
+        target.org_name = source.org_name
+        target.picklist_name = source.picklist_name
+        target.exclude_from_website = source.exclude_from_website
+        target.is_defunct = source.is_defunct
+        target.is_member = source.is_member
+        if source.member_type is not None:
+            target.member_type = source.member_type
+        else:
+            target.member_type = ""
+        target.business_member_level = source.business_member_level
+        target.sector = source.sector
+        target.org_type = source.org_type
+        if source.carnegie_class is not None:
+            target.carnegie_class = source.carnegie_class
+        else:
+            target.carnegie_class = ""
+        if source.class_profile is not None:
+            target.class_profile = source.class_profile
+        else:
+            target.class_profile = ""
+        if source.setting is not None:
+            target.setting = source.setting
+        else:
+            target.setting = ""
+        if source.longitude is not None:
+            target.longitude = source.longitude
+        else:
+            target.longitude = ""
+        if source.latitude is not None:
+            target.latitude = source.latitude
+        else:
+            target.latitude = ""
+        if source.street is not None:
+            target.street = source.street
+        else:
+            target.street = ""
+        if source.city is not None:
+            target.city = source.city
+        else:
+            target.city = ""
+        target.state = source.state
+        if source.postal_code is not None:
+            target.postal_code = source.postal_code
+        else:
+            target.postal_code = ""
+        target.country = source.country
+        if source.country_iso is not None:
+            target.country_iso = source.country_iso
+        else:
+            target.country_iso = ""
+        if source.website is not None:
+            target.website = source.website
+        else:
+            target.website = ""
+        if source.sustainability_website is not None:
+            target.sustainability_website = source.sustainability_website
+        else:
+            target.sustainability_website = ""
+        target.enrollment_fte = source.enrollment_fte
+        target.stars_participant_status = source.stars_participant_status
+        target.pilot_participant = source.pilot_participant
+        target.is_signatory = source.is_signatory
 
-        target_org.save()
+        target.save()
