b1bd07038b0c6a6d801e686372996b3478c71af9
rerb <bob.erb@aashe.org>
2016-02-02 10:06:44 -0500

    Add --include-aashe-in-website flag to org upsert

--- iss/management/commands/upsert_iss_organizations.py
+++ iss/management/commands/upsert_iss_organizations.py
@@ -6,6 +6,7 @@ import os
 
 from django.core.management.base import BaseCommand
 
+import iss.models
 import iss.salesforce
 import iss.utils
 
@@ -22,16 +23,33 @@ class Command(BaseCommand):
             metavar='n-days',
             default=7,
             help='upsert organizations for accounts modified within n-days')
+        parser.add_argument(
+            '-i', '--include-aashe-in-website',
+            action='store_true',
+            help='force AASHE exclude_from_website to be False')
 
     def handle(self, *args, **options):
         upsert_organizations_for_recently_modified_accounts(
-            options['modified_within'])
+            since=options['modified_within'],
+            include_aashe_in_website=options['include_aashe_in_website'])
 
 
-def upsert_organizations_for_recently_modified_accounts(since=7):
-    """Upsert organizations for SF Accounts modified in last `since` days."""
+def upsert_organizations_for_recently_modified_accounts(
+        since=7, include_aashe_in_website=False):
+    """Upsert organizations for SF Accounts modified in last `since` days.
+
+    When `include_aashe_in_website` is true, set the
+    `exclude_from_website` flag on the Organization representing AASHE
+    to False (0, actually).  (Added for the Hub project.)
+    """
     logger.info('upserting orgs for accounts modified in last {since} days'.
                 format(since=since))
     recently_modified_accounts = (
         iss.salesforce.Account.get_recently_modified_accounts(since=since))
     iss.utils.upsert_organizations_for_accounts(recently_modified_accounts)
+
+    if include_aashe_in_website:
+        aashe = iss.models.Organization.objects.get(org_name="AASHE")
+        if aashe.exclude_from_website:
+            aashe.exclude_from_website = 0
+            aashe.save()
