a9ed6bdcd2db7ba11a6caaf732118452f8ab2e00
Benjamin Stookey <ben.stookey@gmail.com>
2017-03-27 10:14:16 -0600

    Added 1.4.3 management commands;

--- iss/management/commands/upsert_iss_memberships_django14.py
+++ iss/management/commands/upsert_iss_memberships_django14.py
@@ -4,35 +4,28 @@ from django.core.serializers import serialize
 from optparse import make_option
 import re
 
-from .upsert_iss_organizations import (
-    upsert_organizations_for_recently_modified_accounts)
+from .upsert_iss_memberships import upsert_memberships_recently_modified
 
 
 class Command(BaseCommand):
     args = '<timeframe>'
-    help = ('Upserts orgs from Membersuite')
+    help = ('Upsert Memberships modified in last `since` days')
     option_list = BaseCommand.option_list + (
         make_option(
             '--all',
             dest='a',
             default=True,
-            help='upsert all organizations'),
+            help='upsert all memberships'),
         make_option(
             '-m',
             type=int,
             dest='m',
             default='7',
-            help='upsert organizations for accounts modified within n-days'),
-        make_option(
-            '-i',
-            dest='i',
-            action='store_true',
-            help='force AASHE exclude_from_website to be False'),
+            help='upsert memberships modified within n-days'),
     )
 
     def handle(self, *args, **options):
-        upsert_organizations_for_recently_modified_accounts(
+        upsert_memberships_recently_modified(
             since=options['m'],
-            include_aashe_in_website=options['i'],
             get_all=options['a'],
         )
