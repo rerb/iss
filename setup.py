f59c527c16e5a4cb62d98641623c863599e64724
Scott Johnson <baronvonvaderham@gmail.com>
2017-04-12 15:45:50 -0400

    Version Bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='iss',
-    version='2.3',
+    version='2.4',
     description="Ideally Single Source app for Salesforce data.",
     author='Scott Johnson',
     author_email='scott@aashe.org',
