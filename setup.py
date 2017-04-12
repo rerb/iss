419d581d05ca764a893500c777c8ccb14f72455e
Robert Erb <bob.erb@gmail.com>
2017-04-11 21:48:38 -0400

    Rm definition of Domain.name

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='iss',
-    version='1.1.1',
+    version='1.1.2',
     description="Ideally Single Source app for Salesforce data.",
     author='Bob Erb',
     author_email='bob.erb@aashe.org',
