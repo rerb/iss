359dd56dcb8b80859e1375caec3bec72139d1026
Robert Erb <bob.erb@gmail.com>
2017-07-26 15:50:20 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name='iss',
-    version='2.5.1',
+    version='2.5.3',
     description="Ideally Single Source app for Salesforce data.",
     author='AASHE',
     author_email='it@aashe.org',
