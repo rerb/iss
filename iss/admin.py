af24b01942edc6b181ee70929af7e16159875fff
Tylor Dodge <tylor@tylorbook.local>
2018-01-30 17:06:32 -0500

    Adding fields to ISS admin list

--- iss/admin.py
+++ iss/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from .models import Organization
 
 class OrganizationAdmin(admin.ModelAdmin):
-    fields = ('account_num', 'org_name', 'city', 'state', 'country_iso')
+    list_display = ('account_num', 'org_name', 'city', 'state', 'country_iso')
+    search_fields = ('org_name', 'account_num')
 admin.site.register(Organization, OrganizationAdmin)
