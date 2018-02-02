4a1bcf28f7225db35b92e14ae7a26f02e408fe36
Tylor Dodge <tylor@tylorbook.local>
2018-02-02 15:41:29 -0500

    Allow for search on Membersuite ID

--- iss/admin.py
+++ iss/admin.py
@@ -4,5 +4,5 @@ from .models import Organization
 class OrganizationAdmin(admin.ModelAdmin):
     list_display = ('membersuite_id', 'account_num', 'org_name', 'city',
         'state', 'country_iso')
-    search_fields = ('org_name', 'membersuite_id', 'account_num')
+    search_fields = ('org_name', '=membersuite_id')
 admin.site.register(Organization, OrganizationAdmin)
