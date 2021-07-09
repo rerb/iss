dc40af76054678ec5804971f7b216b0ae2e6d172
Jared Trouth <jtrouth@burwood.com>
2021-07-09 22:08:01 +0000

    https://docs.djangoproject.com/en/1.8/howto/custom-management-commands/#django.core.management.BaseCommand.option_list

--- iss/management/commands/upsert_iss_organizations_django14.py
+++ iss/management/commands/upsert_iss_organizations_django14.py
@@ -11,19 +11,33 @@ from .upsert_iss_organizations import (
 class Command(BaseCommand):
     args = '<timeframe>'
     help = ('Upserts orgs from Membersuite')
-    option_list = BaseCommand.option_list + (
-        make_option(
-            '-m', '--modified-within',
+    # option_list = BaseCommand.option_list + (
+    #     make_option(
+    #         '-m', '--modified-within',
+    #         type=int,
+    #         dest='m',
+    #         default=None,
+    #         help='upsert organizations for accounts modified within n-days'),
+    #     make_option(
+    #         '-i', '--include-aashe-in-website',
+    #         dest='i',
+    #         action='store_true',
+    #         help='force AASHE exclude_from_website to be False'),
+    # )
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--all',
+            dest='a',
+            default=True,
+            help='upsert all memberships')
+
+        parser.add_argument(
+            '-m',
             type=int,
             dest='m',
-            default=None,
-            help='upsert organizations for accounts modified within n-days'),
-        make_option(
-            '-i', '--include-aashe-in-website',
-            dest='i',
-            action='store_true',
-            help='force AASHE exclude_from_website to be False'),
-    )
+            default='7',
+            help='upsert memberships modified within n-days')
 
     def handle(self, *args, **options):
         upsert_organizations_for_recently_modified_accounts(
